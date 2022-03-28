fc83b98913dd06ce148cedfaaa536dcde838c2d8
KseniaSafonova <kseniatsn@gmail.com>
2022-03-28 11:31:45 +0300

    add classes

--- badbatch/src/Card.jsx
+++ badbatch/src/Card.jsx
@@ -3,14 +3,15 @@ import styles from './Card.module.css';
 
 function Card(props) {
     return (
-        // <div className={'card' + { isSelected? 'selected':  }}>
+        // <div className={`${styles.card}  ${styles.selected}`}> add multiple classes
         <div className={styles.card}>
-            < div className={styles.title} > {props.title}</div >
-            <div className={styles.imageBlock}><img className={styles.image} src={props.imgLink} alt={props.title} /></div>
-            <div className={styles.content}>{props.content}</div>
-        </div >
+            <div className={props.isSelected ? styles.selected : ''}>
+                < div className={styles.title} > {props.title}</div >
+                <div className={styles.imageBlock}><img className={styles.image} src={props.imgLink} alt={props.title} /></div>
+                <div className={styles.content}>{props.content}</div>
+            </div >
+        </div>
     );
-
 }
 
 export default Card;
